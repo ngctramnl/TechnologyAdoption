259189644be441e4495c8c4226bf32e070aff58d
Nguyen Le Ngoc Tram <134762877+ngctramnl@users.noreply.github.com>
2023-07-16 17:46:10 +0700

    Update Techadoption.R

--- Techadoption.R
+++ Techadoption.R
@@ -5,12 +5,23 @@ mydata <- read.csv(file.choose())
 ggplot(mydata, aes(x= Gender, y=LikelinessToAdopt, fill = Gender)) 
 + geom_boxplot() + theme_classic()
 
+#Develop Hypothesis
+
+#H0: There is no significant difference in LikelinessToAdopt between males and females.
+
+#H1: There is a significant difference in LikelinessToAdopt between males and females.
+
+
 #T-test
 t.test(LikelinessToAdopt ~ Gender, data =mydata)
 
 #Create plot LikelinessToAdopt by Education Level:
 ggplot(mydata, aes(x= EducationLevel, y=LikelinessToAdopt, fill = EducationLevel))
 + geom_boxplot() + theme_classic()
+
+#H0: There is no significant difference in LikelinessToAdopt among education-level groups.
+
+#H1: There is a significant difference in LikelinessToAdopt among education-level groups.
 
 #ANOVA Test
 anova1 <- aov(LikelinessToAdopt ~ EducationLevel, data = mydata)
